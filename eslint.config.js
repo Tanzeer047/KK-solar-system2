f41e2d9e01e0522f2e681537a7cfee4255a64c7f
Tanzeer047 <tanzeerahamed047@gmail.com>
2025-02-20 10:37:53 +0530

    Create eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+export default [
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: "module",
+    },
+    rules: {
+      "indent": ["error", 2],
+      "linebreak-style": ["error", "unix"],
+      "quotes": ["error", "single"],
+      "semi": ["error", "always"],
+    },
+  },
+];
